d07cb7705d9b383ac13d2527b351742b9da31e8b
SeleckyErik <35656626+SeleckyErik@users.noreply.github.com>
2020-02-02 14:55:05 +0000

    Add checklist to main scene Add on_level_finished to main scene Add comments to Level0 for Eric

--- scenes/levels/Level0.gd
+++ scenes/levels/Level0.gd
@@ -3,7 +3,7 @@ extends LevelBase
 onready var timer: = $Timer as Timer
 
 func create_level_setup() -> LevelSetup:
-	var setup: = LevelSetup.new()
+	var setup: = LevelSetup.new() # stup the level. WHich sockets and cables are available
 	
 	setup.sockets[0] = true
 	setup.sockets[1] = true
@@ -13,18 +13,17 @@ func create_level_setup() -> LevelSetup:
 	return setup
 
 
-func _process(_delta) -> void:
-	if not complete:
+func _process(_delta) -> void: #similiar to tick in UE4
+	if not complete: #if complete false play level
 		if main_scene.sockets[0].state == Socket.State.CONNECTED: 
 			if main_scene.sockets[1].state == Socket.State.CONNECTED:
+				complete = true #finish state if both connected
 				emit_signal("level_complete")
-				complete = true
-				print("Done!")
 
 func _ready() -> void:
-	timer.start(1.5)
+	timer.start(1.5) #seconds until the first call is starting
 	timer.connect("timeout", self, "_timer_finished")
 
 
 func _timer_finished() -> void:
-	main_scene.sockets[0].ring(main_scene.sockets[1])
+	main_scene.sockets[0].ring(main_scene.sockets[1])  #### Call from socket 0 to socket 1
