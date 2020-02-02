667ffd881efcc9aee58bd28113e2352a32d056ce
SeleckyErik <35656626+SeleckyErik@users.noreply.github.com>
2020-02-02 13:52:31 +0000

    Add checklist prefab

---
+++ scenes/prefabs/Checklist.gd
@@ -0,0 +1,33 @@
+extends Spatial
+
+var calls: = Array()
+
+onready var rtlabel = $Viewport/Control/MarginContainer/RichTextLabel
+
+func add_call(from: String, to: String) -> int:
+	var new_entry: = "{0} calling {1}".format([from, to])
+	calls.append(new_entry)
+	_reconstruct_text()
+
+	return calls.size() - 1
+
+func call_complete(idx: int):
+	if idx >= 0 and idx < calls.size():
+		calls[idx] = "[s]%s[/s]" % calls[idx]
+		_reconstruct_text()
+
+
+func _ready() -> void:
+	_reconstruct_text()
+	
+	add_call("A1", "B1")
+	add_call("A1", "A2")
+	call_complete(0)
+
+
+func _reconstruct_text():
+	var bbtex: = ""
+	for i in range(calls.size()):
+		bbtex = bbtex + calls[i] + "\n"
+	rtlabel.bbcode_text = bbtex
+	rtlabel.bbcode_text
