46afd1364ffa70b1471d2c428f022ec201270a1e
SeleckyErik <35656626+SeleckyErik@users.noreply.github.com>
2020-02-02 02:06:05 +0000

    Make cable draggable Make cable rotate towards socket

--- scripts/WireSystem.gd
+++ scripts/WireSystem.gd
@@ -10,7 +10,7 @@ var plug_query_dir = null
 var board_size: Vector2
 var plug_mouse_offset := Vector3.ZERO
 
-var hover_threshold = 0.7
+var hover_threshold = 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -29,7 +29,9 @@ func project_mouse_ray_onto_plane() -> Vector3:
 	var query_dir = camera.project_ray_normal(mouse_pos)
 	var board = Plane(Vector3(0, 0, 1), 0.0)
 	var new_plug_pos = board.intersects_ray(query_pos, query_dir)
+	
 	return new_plug_pos
+
 
 
 func _process(delta):
@@ -45,24 +47,37 @@ func _process(delta):
 			if hovered_socket != null:
 				selected_plug.set_attached(true)
 				hovered_socket.plug_in(selected_plug)
+				selected_plug.rotation = Vector3(PI/2, 0, 0)
+			else:
+				selected_plug.set_attached(false)
+				selected_plug.rotation = Vector3.ZERO
 			selected_plug = null
 	
-	if selected_plug != null:
+	if selected_plug != null:	
 		if hovered_socket != null:
-			hovered_socket.set_hovered(false)
+			hovered_socket.set_state(0)
+			hovered_socket.unplug()
 			hovered_socket = null
 		var new_plug_pos = project_mouse_ray_onto_plane()
 		if new_plug_pos != null:
-			selected_plug.set_position(Vector3(new_plug_pos.x, new_plug_pos.y, selected_plug.get_position().z) + plug_mouse_offset)
+			
+			selected_plug.set_position(Vector3(new_plug_pos.x, new_plug_pos.y, selected_plug.get_position().z))
 			for socket in all_sockets:
 				var dist = socket.global_transform.origin.distance_to(selected_plug.get_position())
-				print(dist)
+				#print(dist)
 				if dist < hover_threshold:
 					hovered_socket = socket
-					hovered_socket.set_hovered(true)
-					selected_plug.global_transform.looking_at(hovered_socket.global_transform.origin, Vector3.UP)
+					hovered_socket.set_state(1)
+					
+					selected_plug.global_transform = selected_plug.global_transform.looking_at(hovered_socket.global_transform.origin, Vector3.UP)
+					
+					var z_rot: = 90.0
+					if (hovered_socket.global_transform.origin - selected_plug.global_transform.origin).x < 0.0:
+						z_rot = -90.0
+					
+					selected_plug.rotate_z(z_rot)
 					break
-			print()
+			#print()
 
 func _unhandled_input(event):
 	if event is InputEventKey:
@@ -75,11 +90,11 @@ func _physics_process(delta):
 			var new_plug_pos = plug.query(plug_query_pos, plug_query_dir)
 			if new_plug_pos != Vector3.INF:
 				selected_plug = plug
+				selected_plug.set_attached(true)
 				var plane_intersection = project_mouse_ray_onto_plane()
 				if plane_intersection != null:
 					plug_mouse_offset = plug.translation - plane_intersection
-					plug_mouse_offset.z = 0
-					print(plug_mouse_offset)
+					#print(plug_mouse_offset)
 					break
 				if selected_plug.plugged_socket != null:
 					hovered_socket.unplug()
