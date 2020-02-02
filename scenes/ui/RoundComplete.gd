5051981ec859f6320aad25e6b4b3f84ebea02a51
Joe Flood <joe@tinygoose.co.uk>
2020-02-02 16:31:36 +0000

    Round complete UI & background

--- scenes/ui/RoundComplete.gd
+++ scenes/ui/RoundComplete.gd
@@ -8,6 +8,12 @@ func _ready():
 
 func _on_click_restart():
 	main_scene.reload_level()
+	$AnimationPlayer.play_backwards("animate_in")
+	yield($AnimationPlayer, "animation_finished")
+	queue_free()
 	
 func _on_click_next_level():
 	main_scene.load_next_level()
+	$AnimationPlayer.play_backwards("animate_in")
+	yield($AnimationPlayer, "animation_finished")
+	queue_free()
